3540997b41cac1f3b0b25f268d4c1508b3fc1fb5
Max Pijittum <mahasak@gmail.com>
2025-04-01 15:16:59 +0700

    change to Remix

---
+++ app/routes/hello._index.tsx
@@ -0,0 +1,20 @@
+import { getServerTiming } from "../timing.server";
+
+export async function loader() {
+  const { time, getServerTimingHeader } = getServerTiming()
+
+  await time(
+    {
+      name: "content",
+      description: "Compile",
+    },
+    // eslint-disable-next-line no-unused-labels
+    () => {},
+  );
+
+ return Response.json(
+  { text: 'Hello' },
+  {
+    headers: getServerTimingHeader(),
+  },);
+}
